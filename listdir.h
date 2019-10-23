dc177bfe6a3eb6a7a913872584ab399c33d130b0
Antonella <antonellalrc@gmail.com>
2019-10-22 21:11:24 -0400

    agrega .h de listdir

---
+++ listdir.h
@@ -0,0 +1,20 @@
+/*
+* Encabezado de libreria de listar directorio
+* --------------------
+* Autores: Antonella Requena 15-11196
+*         Ezequiel Zabala 14-11160
+*
+* Ultima fecha de modificacion: 22/09/2019
+*/
+
+/* Definiciones */
+#ifndef LISTDIR_H_
+#define LISTDIR_H_
+
+/* *********** Declaracion de variables ********** */
+
+/* ************** Declaraciones de Funciones *************** */
+void listdir(const char *name, int indent);
+
+
+#endif
