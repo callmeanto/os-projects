769e1b61ae78ca969b996baf4188cad974e20e5c
ezequielzu <ezequielzabalau@gmail.com>
2019-11-03 16:25:54 -0400

    cifrado listo... unless..?

--- cifradoejemplo.c
+++ cifradoejemplo.c
@@ -5,51 +5,105 @@
 #define MAX 50
 
 /*PARA CIFRAR*/
-unsigned char cshiftR(unsigned char x, int m)
-{ /*shift circular a la derecha m veces al caracter x*/
+unsigned char cshiftR(unsigned char x, int z) 
+{ 
 	int n;
-	if(m<=8)
-		n = m;	
+	if(z<=8)
+		n = z;	
+	/* Si z>8, calculamos z%8 para saber cuántas veces realmente se rota,
+	* pues rotar el byte por múltiplos de 8 resulta en el mismo byte */
 	else{
-		n = m % 8; 
+		n = z % 8; 
 	}
 	return (x >> n) | (x << (8 - n));
 }
-
 /*PARA DESCIFRAR*/
-unsigned char cshiftL(unsigned char x, int m)
-{ /*shift circular a la izquierda m veces al caracter x*/
+unsigned char cshiftL(unsigned char x, int y)
+{
 	int n;
-	if(m<=8)
-		n = m;	
+	if(y<=8)
+		n = y;	
+	/* Si y>8, calculamos y%8 para saber cuántas veces realmente se rota,
+	* pues rotar el byte por múltiplos de 8 resulta en el mismo byte */	
 	else{
-		n = m % 8; 
+		n = y % 8; 
 	}
 	return (x << n) | (x >> (8 - n));
 }
 
-void cifrarArchivo(FILE *fp, int n)
-{ /*recorre fp caracter por caracter, cifrando cada uno (un 1B)*/
-	char c_actual; /*el caracter donde estoy parado*/
-	char c_cif;
-	while ((c_actual = fgetc(fp)) != EOF) /*lee un caracter a la vez mientras no sea EOF*/
+
+int encryptArchive(FILE *fmytar, int n)
+{
+	char c_actual;	/* Variable que almacena el caracter donde estoy parado */
+	char c_cif;		/* Variable que almacena el nuevo caracter cifrado */
+	char bandera_cif[10]; /* Almacena la bandera de cifrado del archivo */
+
+	fscanf(fmytar,"%s",bandera_cif); /* Recibe la bandera del archivo */
+
+	if (strcmp(bandera_cif,"ENCRYPT:1") == 0)
 	{
-		c_cif = cshiftR(c_actual,n); /*el valor de c_actual cifrado*/
-		fseek(fp, ftell(fp) - 1, SEEK_SET); /*devuelve el puntero un caracter para sobreescribirlo*/
-		fputc(c_cif,fp); /*sobreescritura del cifrado sobre el descifrado*/
+		printf("Error: ya el archivo esta cifrado.\n");
+		return 0;
 	}
+
+	fseek(fmytar,8,SEEK_SET);
+	fputc('1',fmytar); /* Actualiza la bandera para indicar que se cifró*/
+	fseek(fmytar, 11, SEEK_SET);
+	/* Lee un caracter a la vez mientras no sea EOF */
+	while ((c_actual = fgetc(fmytar)) != EOF) 
+	{
+		c_cif = cshiftR(c_actual,n); /* Se almacena  el caracter cifrado */
+		fseek(fmytar, ftell(fmytar)-1, SEEK_SET); /* Retrocede el puntero un
+													caracter para sobreescribir*/
+		fputc(c_cif,fmytar); /* Sobreescribe el cifrado sobre el descifrado */
+	}
+	return 1;
 }
 
 
-void descifrarArchivo(FILE *fp, int n)
-{ /*recorre fp caracter por caracter, descifrando cada uno*/
-	char c_actual; /*el caracter donde estoy parado*/
-	char c_desc; 
-	while ((c_actual = fgetc(fp)) != EOF) /*lee un caracter a la vez mientras no sea EOF*/
+int decryptArchive(FILE *fmytar, int n)
+{
+	char c_actual; /* Variable que almacena el caracter donde estoy parado */
+	char c_desc;   /* Variable que almacena el nuevo caracter descifrado */
+	char bandera_cif[10]; /* Almacena la bandera de cifrado del archivo */
+	char *bandera_key; /* Almacena "KEY", si se consigue "KEY" en el archivo*/
+	char *buff; /* Almacena 5 bytes del archivo para buscar "KEY" luego*/
+
+	fscanf(fmytar,"%s",bandera_cif); /* Recibe la bandera del archivo */
+
+	if (strcmp(bandera_cif,"ENCRYPT:0") == 0)
 	{
-		c_desc = cshiftL(c_actual,n); /*el verdadero valor de c_actual*/
-		fseek(fp, ftell(fp) - 1, SEEK_SET); /*devuelve el puntero un caracter para sobreescribirlo*/
-		fputc(c_desc,fp); /*sobreescritura del descifrado sobre el cifrado*/
+		printf("Error: ya el archivo esta descifrado.\n");
+		return 0;
+	}
+
+	fseek(fmytar,8,SEEK_SET);
+	fputc('0',fmytar); /* Actualiza la bandera para indicar que se descifró*/
+	fseek(fmytar, 11, SEEK_SET);
+	/* Lee un caracter a la vez mientras no sea EOF */
+	while ((c_actual = fgetc(fmytar)) != EOF)
+	{
+		c_desc = cshiftL(c_actual,n); /* Se almacena  el caracter descifrado */
+		fseek(fmytar, ftell(fmytar)-1, SEEK_SET); /* Retrocede el puntero un
+													caracter para sobreescribir */
+		fputc(c_desc,fmytar); /* Sobreescribe el descifrado sobre el cifrado */
+	}
+
+	bandera_key = "KEY";
+
+	/* Mueve el puntero a un punto después de la bandera de cifrado */
+	fseek(fmytar,11,SEEK_SET);
+	fgets(buff,5,fmytar); /* almacena los siguientes 5 chars, buscamos "KEY" */
+
+	if (strstr(buff,bandera_key)) /* Si se consigue "KEY" en buff, bien */
+	{
+		return 1;
+	}
+	else
+	{
+		/* si no se consigue "KEY" la rotación de bits no descifró el archivo */
+		printf("Error: los bits rotados no coinciden con los del cifrado.\n");
+		return 0;
 	}
 }
 
@@ -85,16 +139,29 @@ int main ()
 		printf("Error: opcion desconocida.\nSolo 'z'/'Z' y 'y'/'Y' son validos.\n");
 		exit(-1); /*Si se escoge un caracter invalido, termina el programa*/
 	}
-	printf("FLAGS: z=%d y=%d\n",zflag,yflag);
+	/*printf("FLAGS: z=%d y=%d\n",zflag,yflag);*/
 
 	printf("Escoja la llave de cifrado: ");
 	scanf("%d",&n);
 
 	if (zflag)
-		cifrarArchivo(fp,n);
+		if (encryptArchive(fp,n))
+			printf("Cifrado fabuloso!");
+		else
+		{
+			printf("Cifrado desastroso!");
+			fclose(fp);
+			return -1;
+		}
 	if (yflag)
-		descifrarArchivo(fp,n);
-
+		if (decryptArchive(fp,n)==1)
+			printf("Descifrado asombroso!");
+		else
+		{
+			printf("Descifrado espantoso!");
+			fclose(fp);
+			return -1;
+		}
 
 	fclose(fp);
 	return 0;
--- encrypt.c
+++ encrypt.c
@@ -58,16 +58,29 @@ unsigned char cshiftL(unsigned char x, int y)
 * byte a la derecha n veces. Esto lo logra leyendo los caracteres del archivo
 * uno por uno, rotándolos a la derecha y sustituyendo el caracter original por
 * su versión rotada. Esto funciona pues sabemos que el archivo mytar es de tipo
-* texto, por lo que cada caracter es un solo byte. No retorna nada (tipo void).
+* texto, por lo que cada caracter es un solo byte. Retorna 1 si el cifrado fue
+* exitoso, 0 si se intenta cifrar un archivo cifrado (error).
 *
 * fmytar: apuntador al archivo mytar 
 * n: cantidad de veces que se rotan los bits de cada byte a la derecha
 */
-void encryptArchive(FILE *fmytar, int n)
+int encryptArchive(FILE *fmytar, int n)
 {
 	char c_actual;	/* Variable que almacena el caracter donde estoy parado */
 	char c_cif;		/* Variable que almacena el nuevo caracter cifrado */
+	char bandera_cif[10]; /* Almacena la bandera de cifrado del archivo */
 
+	fscanf(fmytar,"%s",bandera_cif); /* Recibe la bandera del archivo */
+
+	if (strcmp(bandera_cif,"ENCRYPT:1") == 0)
+	{
+		printf("Error: ya el archivo esta cifrado.\n");
+		return 0;
+	}
+
+	fseek(fmytar,8,SEEK_SET);
+	fputc('1',fmytar); /* Actualiza la bandera para indicar que se cifró*/
+	fseek(fmytar, 11, SEEK_SET);
 	/* Lee un caracter a la vez mientras no sea EOF */
 	while ((c_actual = fgetc(fmytar)) != EOF) 
 	{
@@ -76,10 +89,11 @@ void encryptArchive(FILE *fmytar, int n)
 													caracter para sobreescribir*/
 		fputc(c_cif,fmytar); /* Sobreescribe el cifrado sobre el descifrado */
 	}
+	return 1;
 }
 
 /*
-* Funcion: Cifrado del archivo tar
+* Funcion: Descifrado del archivo tar
 * --------------------
 * Si la opción -y fue marcada en la entrada, esta función descifra el archivo 
 * mytar antes de extraer su contenido total o parcialmente. El descifrado
@@ -88,16 +102,32 @@ void encryptArchive(FILE *fmytar, int n)
 * sustituyendo el caracter cifrado por su versión descifrada. Esto funciona
 * pues sabemos que el archivo mytar es de tipo texto, por lo que cada caracter
 * es un solo byte. Si la cantidad de rotaciones no coincide con las rotaciones
-* del cifrado, el archivo seguirá cifrado. No retorna nada (tipo void).
+* del cifrado, el archivo seguirá cifrado.  Retorna 1 si el descifrado fue
+* exitoso, 0 si se intenta descifrar un archivo descifrado (error), -1 si los
+* bits a rotar no coinciden con la rotación del cifrado (error).
 *
 * fmytar: apuntador al archivo mytar 
 * n: cantidad de veces que se rotan los bits de cada byte a la izquierda
 */
-void decryptArchive(FILE *fmytar, int n)
+int decryptArchive(FILE *fmytar, int n)
 {
 	char c_actual; /* Variable que almacena el caracter donde estoy parado */
 	char c_desc;   /* Variable que almacena el nuevo caracter descifrado */
+	char bandera_cif[10]; /* Almacena la bandera de cifrado del archivo */
+	char *bandera_key; /* Almacena "KEY", si se consigue "KEY" en el archivo*/
+	char *buff; /* Almacena 5 bytes del archivo para buscar "KEY" luego*/
 
+	fscanf(fmytar,"%s",bandera_cif); /* Recibe la bandera del archivo */
+
+	if (strcmp(bandera_cif,"ENCRYPT:0") == 0)
+	{
+		printf("Error: ya el archivo esta descifrado.\n");
+		return 0;
+	}
+
+	fseek(fmytar,8,SEEK_SET);
+	fputc('0',fmytar); /* Actualiza la bandera para indicar que se descifró*/
+	fseek(fmytar, 11, SEEK_SET);
 	/* Lee un caracter a la vez mientras no sea EOF */
 	while ((c_actual = fgetc(fmytar)) != EOF)
 	{
@@ -105,5 +135,22 @@ void decryptArchive(FILE *fmytar, int n)
 		fseek(fmytar, ftell(fmytar)-1, SEEK_SET); /* Retrocede el puntero un
 													caracter para sobreescribir */
 		fputc(c_desc,fmytar); /* Sobreescribe el descifrado sobre el cifrado */
+	}
+
+	bandera_key = "KEY";
+
+	/* Mueve el puntero a un punto después de la bandera de cifrado */
+	fseek(fmytar,11,SEEK_SET);
+	fgets(buff,5,fmytar); /* almacena los siguientes 5 chars, buscamos "KEY" */
+
+	if (strstr(buff,bandera_key)) /* Si se consigue "KEY" en buff, bien */
+	{
+		return 1;
+	}
+	else
+	{
+		/* si no se consigue "KEY" la rotación de bits no descifró el archivo */
+		printf("Error: los bits rotados no coinciden con los del cifrado.\n");
+		return 0;
 	}
 }
