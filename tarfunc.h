92c5e10fc651605f9c11d96a5b098d9ec326b732
Antonella <antonellalrc@gmail.com>
2019-11-03 17:36:05 -0400

    agrega libreria con funciones del tar

---
+++ tarfunc.h
@@ -0,0 +1,18 @@
+/*
+* Encabezado de libreria de funciones del mytar
+* --------------------
+* Autores: Antonella Requena 15-11196
+*          Ezequiel Zabala 14-11160
+*
+* Ultima fecha de modificacion: 3/11/2019
+*/
+
+/* Definiciones */
+#ifndef TARFUNC_H_
+#define TARFUNC_H_
+
+/* *********** Declaracion de variables ********** */
+
+/* ************** Declaraciones de Funciones *************** */
+
+#endif
